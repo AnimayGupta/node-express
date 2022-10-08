0ba4c9e1b000e02ff56ceefad9a152f5e305868b
Animay Gupta <animay.gupta@ab-inbev.com>
2022-10-08 17:52:31 +0530

    commit on 8th Oct

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,53 +2,28 @@ const express = require('express');
 const path = require('path')
 const app = express()
 const {products} = require('./data.js')
+const logger = require('./logger')
+const authorize = require('./authorize')
 // app.use(express.static('./public'))
+// req => middleware => res
+app.use([authorize, logger])
+
+
 
 app.get('/', (req,res) => {
-    res.send('<h1> Home Page </h1> <a href = "/api/products">Products</a>')
+    res.send('Home')
+})
+
+app.get('/about', (req,res) => {
+    res.send('About')
 })
 
 app.get('/api/products', (req,res) => {
-    const newProducts = products.map((product) => {
-        const {id,name,image} = product
-        return {id,name,image}
-    })
-    res.json(newProducts)
+    res.send('Products')
 })
-
-app.get('/api/products/:productID', (req,res) => {
-    // console.log(req.params)
-    const {productID} = req.params
-    const singleProduct = products.find((product) => product.id === Number(productID))
-    if(!singleProduct){
-        res.status(404).send('Product does not exist')
-    }else{
-        res.json(singleProduct)
-    }
+app.get('/api/items', (req,res) => {
+    res.send('Items')
 })
-
-app.get('/api/v1/query', (req,res) => {
-    // console.log(req.query)
-    const {search,limit} = req.query
-    let sortedProducts = [...products]
-    if(search){
-        sortedProducts = sortedProducts.filter((product) => {
-            return product.name.startsWith(search)
-        })
-    }
-    if(limit){
-        sortedProducts = sortedProducts.slice(0,Number(limit))
-    }
-    if(sortedProducts.length < 1){
-       return res.status(200).json({success: true, data: []})
-    }
-    return res.status(200).json(sortedProducts)
-    // res.send('hello world')
-})
-
-// app.all('*', (req,res) => {
-//     res.status(404).send('Resource not found')
-// })
 
 app.listen(5000, () => {
     console.log('server is listening on port 5000...')
