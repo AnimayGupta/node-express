791d5a941ef8050def5e9ea1d5d255cc8584d306
Animay Gupta <animay.gupta@ab-inbev.com>
2022-10-09 02:07:51 +0530

    commit on 9th Oct

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,29 +1,15 @@
 const express = require('express');
 const path = require('path')
 const app = express()
-const {products} = require('./data.js')
-const logger = require('./logger')
-const authorize = require('./authorize')
-// app.use(express.static('./public'))
-// req => middleware => res
-app.use([authorize, logger])
+const people = require('./routes/people')
+const auth = require('./routes/auth')
 
 
-
-app.get('/', (req,res) => {
-    res.send('Home')
-})
-
-app.get('/about', (req,res) => {
-    res.send('About')
-})
-
-app.get('/api/products', (req,res) => {
-    res.send('Products')
-})
-app.get('/api/items', (req,res) => {
-    res.send('Items')
-})
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({extended : false}))
+app.use(express.json())
+app.use('/api/people', people)
+app.use('/login', auth)
 
 app.listen(5000, () => {
     console.log('server is listening on port 5000...')
